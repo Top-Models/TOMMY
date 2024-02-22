e971e9c4cef7d66c2e261b5ea252b44d3c989724
TemporarilyTired <66777276+TemporarilyTired@users.noreply.github.com>
2024-02-22 16:08:40 +0100

    Initialized Gensim LDA model class as a subclass of the abstract model

--- interactive_topic_modeling/backend/model/lda_model.py
+++ interactive_topic_modeling/backend/model/lda_model.py
@@ -1,5 +1,25 @@
+from gensim.corpora.dictionary import Dictionary
+from gensim.models.ldamodel import LdaModel
+from itertools import chain
+from interactive_topic_modeling.backend.model.abstract_model import Model
 
-class LdaModel:
 
-    def __init(self):
-        pass
+class GensimLdaModel(Model):
+
+    def __init__(self, docs, k):
+        self.dictionary: Dictionary
+        self.model: LdaModel
+        self.train_model(docs, k)
+
+    def train_model(self, docs, k):
+        self.dictionary = Dictionary(docs)
+        self.corpus = [self.dictionary.doc2bow(tokens) for tokens in docs]
+        self.model = LdaModel(corpus=self.corpus, id2word=self.dictionary, num_topics=k)
+
+    def update_model(self, docs):
+        added_corpus = [self.dictionary.doc2bow(tokens) for tokens in docs]
+        self.model.update(added_corpus)
+        self.corpus = chain(self.corpus, added_corpus)
+
+    def get_topics(self):
+         return self.model.show_topics(formatted=False)
