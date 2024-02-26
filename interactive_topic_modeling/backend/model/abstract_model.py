b68f99bfadcf757a5fd840f4ec508c81d6d91918
TemporarilyTired <66777276+TemporarilyTired@users.noreply.github.com>
2024-02-26 16:14:50 +0100

    Added more get-functions to abstract Model Added Save and load functions to abstract Model WITHOUT implementations in GensimLdaModel

--- interactive_topic_modeling/backend/model/abstract_model.py
+++ interactive_topic_modeling/backend/model/abstract_model.py
@@ -1,18 +1,35 @@
 # This file defines an absract base class that all topic modelling algorithms will implement
-# The class will define a function to take a preprocessed input corpus
-# as an iterable that yields another iterable which in turn yield the token in string format
+# The class will define a function to take a preprocessed input corpus, i.e., an iterable of lists of strings
 
 from random import randrange
 
 from abc import ABC, abstractmethod
 from collections.abc import Iterable
 from typing import Tuple, List, NewType
+from numpy import ndarray  # todo: add numpy to project requirements
+from os import path
 
-Doc = NewType('Doc', List[str])
-Docs = NewType('Docs', Iterable[Doc])
+Term = NewType('Term', str)
+TermList = NewType('TermList', List[Term])
+TermLists = NewType('TermLists', Iterable[TermList])
 
 
 class Model(ABC):
+    _num_topics: int
+
+    @property
+    def num_topics(self):
+        return self._num_topics
+
+    @num_topics.setter
+    def num_topics(self, new_num_topics: int):
+        if new_num_topics is None:
+            return
+        elif isinstance(new_num_topics, int):
+            self._random_seed = new_num_topics
+        else:
+            raise ValueError('Number of topics passed to Model class is not an integer.', new_num_topics)
+
     _random_seed: int
 
     @property
@@ -28,29 +45,73 @@ class Model(ABC):
         else:
             raise ValueError('Random seed passed to Model class is not an integer.', new_random_seed)
 
-#    @abstractmethod
-#    def __init__(self, docs: Docs, k: int, random_seed: int):
     def __init__(self, random_seed: int):
         self.random_seed = random_seed
 
     # Trains the model from scratch on the input iterable of iterables of tokens with k topics
     @abstractmethod
-    def train_model(self, docs: Docs, k: int):
+    def train_model(self, term_lists: TermLists, num_topics: int = None):
         pass
 
     # Updates the model to include the information from the input iterable of iterables of tokens
     @abstractmethod
-    def update_model(self, docs: Docs):
+    def update_model(self, term_lists: TermLists):
         pass
 
-    # Returns the found topics in the format where each topic consists of a list of
-    # tuples of the word and the score of that word, e.g., [(_topid_id, [('word', 0.52), ('ortoken', 0.412)])]
+    # Returns the term that belongs to the given term_id
     @abstractmethod
-    def get_topics(self) -> List[Tuple[int, List[Tuple[str, float]]]]:
+    def get_term(self, term_id):
         pass
 
-    # Returns a list with for each topic that is somewhat present in the document,
+    # Returns the number of unique terms that have been received by the model
+    @abstractmethod
+    def n_terms(self) -> int:
+        pass
+
+    # Returns the found topics in the format where each topic consists of a list of for all top n terms the
+    # tuples of the term and the score of that word, e.g., [(_topic_id, [('word', 0.52), ('etc', 0.412)])]
+    @abstractmethod
+    def show_topics(self, n) -> List[Tuple[int, List[Tuple[Term, float]]]]:
+        pass
+
+    # Returns the found topics in the format where each topic consists of a list of for all top n terms the
+    # tuples of the term_id and the score of that word, e.g., [(_topic_id, [(42, 0.52), (58, 0.412)])]
+    @abstractmethod
+    def get_topics(self, n) -> List[Tuple[int, List[Tuple[int, float]]]]:
+        pass
+
+    # Returns for the topic associated with the topic_id, a list of for all top n words the
+    # tuples of the word and the score of that word, e.g., [(_topic_id, [('word', 0.52), ('etc', 0.412)])]
+    @abstractmethod
+    def show_topic_terms(self, topic_id, n) -> List[Tuple[Term, float]]:
+        pass
+
+    # Returns for the topic associated with the topic_id, a list of for all top n terms the
+    # tuples of the termID and the score of that word, e.g., [(_topic_id, [(42, 0.52), (58, 0.412)])]
+    @abstractmethod
+    def get_topic_terms(self, topic_id, n) -> List[Tuple[int, float]]:
+        pass
+
+    # Returns a list with for each topic that is at least with minimum_probability present in the document,
     # (the topic ID, and) the float (0 to 1) representing how much that topic is present in this document
     @abstractmethod
-    def get_doc_topics(self, doc: Doc) -> list[tuple[int, float]]:
+    def get_doc_topics(self, term_list: TermList, minimum_probability=0) -> list[tuple[int, float]]:
+        pass
+
+    # Returns a numpy array of size n_topics x n_terms, where the values represent the probability for each
+    # term in each topic.
+    @abstractmethod
+    def get_topic_term_numpy_matrix(self) -> ndarray:
+        pass
+
+    # Saves the internal state of the model to the location on the hard disk specified by fpath
+    @abstractmethod
+    def save(self, fpath: path):
+        pass
+
+    # Loads a Model from an earlier saved internal state. This method returns an instance of the Model.
+    # So use for example: "my_lda_model = GensimLdaModel.load(os.curdir)"
+    @classmethod
+    @abstractmethod
+    def load(cls, fpath: path):
         pass
--- interactive_topic_modeling/backend/model/lda_model.py
+++ interactive_topic_modeling/backend/model/lda_model.py
@@ -1,4 +1,4 @@
-from interactive_topic_modeling.backend.model.abstract_model import Model, Doc, Docs
+from interactive_topic_modeling.backend.model.abstract_model import Model, TermLists
 
 from gensim.corpora.dictionary import Dictionary
 from gensim.models.ldamodel import LdaModel
@@ -12,28 +12,56 @@ from typing import Tuple
 class GensimLdaModel(Model):
     dictionary: Dictionary
     model: LdaModel
-    corpus: Iterable[Iterable[Tuple[int, int]]]
+    bags_of_words: Iterable[Iterable[Tuple[int, int]]]
 
-    def __init__(self, docs: Docs, k: int, random_seed=None):
+    def __init__(self, term_lists: TermLists, num_topics: int, random_seed=None):
         super().__init__(random_seed)
-        self.train_model(docs, k)
+        self.num_topics = num_topics
+        self.train_model(term_lists)
 
-    def train_model(self, docs, k):
+    def train_model(self, docs, num_topics=None):
+        self.num_topics = num_topics
+
         self.dictionary = Dictionary(docs)
-        self.corpus = [self.dictionary.doc2bow(tokens) for tokens in docs]
-        self.model = LdaModel(corpus=self.corpus,
+        self.bags_of_words = [self.dictionary.doc2bow(tokens) for tokens in docs]
+        self.model = LdaModel(corpus=self.bags_of_words,
                               id2word=self.dictionary,
-                              num_topics=k,
+                              num_topics=num_topics,
                               random_state=self.random_seed)
 
     def update_model(self, docs):
         added_corpus = [self.dictionary.doc2bow(tokens) for tokens in docs]
         self.model.update(added_corpus)
-        self.corpus = chain(self.corpus, added_corpus)
+        self.bags_of_words = chain(self.bags_of_words, added_corpus)
 
-    def get_topics(self):
-        return self.model.show_topics(formatted=False)
+    def get_term(self, term_id):
+        return self.dictionary[term_id]
 
-    def get_doc_topics(self, doc):
+    def n_terms(self):
+        return len(self.dictionary)
+
+    def show_topics(self, n):
+        return self.model.show_topics(formatted=False, num_words=n)
+
+    def get_topics(self, n):
+        return [(topic_id, ) for topic_id in range(self.num_topics)]
+
+    def show_topic_terms(self, topic_id, n):
+        return self.model.show_topic(topic_id, topn=n)
+
+    def get_topic_terms(self, topic_id, n):
+        return self.model.get_topic_terms(topic_id, topn=n)
+
+    def get_doc_topics(self, doc, minimum_probability=0):
         bag_of_words = self.dictionary.doc2bow(doc)
-        return self.model[bag_of_words]
+        return self.model.get_document_topics(bag_of_words, minimum_probability=minimum_probability)
+
+    def get_topic_term_numpy_matrix(self):
+        return self.model.get_topics()
+
+    def save(self, fpath):
+        raise NotImplementedError("Saving the model has not been implemented in GensimLdaModel")
+
+    @classmethod
+    def load(cls, fpath):
+        raise NotImplementedError("Loading the model has not been implemented in GensimLdaModel")
