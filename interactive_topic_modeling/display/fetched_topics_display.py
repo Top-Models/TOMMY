c3fc1c110b5dfd3b3e93b114d5bd6dc887be7d2f
Belle-and-the-Wolf <91776815+Belle-and-the-Wolf@users.noreply.github.com>
2024-02-27 12:09:55 +0100

    [FEATURE] Implemented scrollable area for fetched topics

--- interactive_topic_modeling/display/fetched_topics_display.py
+++ interactive_topic_modeling/display/fetched_topics_display.py
@@ -10,15 +10,54 @@ class FetchedTopicsDisplay(QScrollArea):
 
         # Initialize widget properties
         self.setMinimumSize(195, 400)
-        self.setStyleSheet("background-color: lightgrey;")
+        self.setStyleSheet("background-color: white;")
 
-        # Initialize layout
-        self.layout = QVBoxLayout()
-        self.setLayout(self.layout)
+        # Initialize layout and scroll area
+        self.widget = QWidget()
+        self.layout = QVBoxLayout(self.widget)
 
-        # Initialize widgets
-        self.label = QLabel("Fetched Topics Display")
-        self.label.setAlignment(Qt.AlignCenter)
+        # Initialize Topics
+        self.first_topic = QWidget()
+        self.first_topic.setStyleSheet("background-color: black;")
+        self.first_topic.setFixedSize(200, 100)
+
+        self.second_topic = QLabel()
+        self.second_topic.setStyleSheet("background-color: pink;")
+        self.second_topic.setFixedSize(200, 100)
+
+        self.third_topic = QLabel()
+        self.third_topic.setStyleSheet("background-color: green;")
+        self.third_topic.setFixedSize(200, 100)
+
+        self.fourth_topic = QLabel()
+        self.fourth_topic.setStyleSheet("background-color: red;")
+        self.fourth_topic.setFixedSize(200, 100)
+
+        self.fifth_topic = QLabel()
+        self.fifth_topic.setStyleSheet("background-color: green;")
+        self.fifth_topic.setFixedSize(200, 100)
 
         # Add widgets
-        self.layout.addWidget(self.label)
+        self.layout.addWidget(self.first_topic)
+        self.layout.addWidget(self.second_topic)
+        self.layout.addWidget(self.third_topic)
+        self.layout.addWidget(self.fourth_topic)
+        self.layout.addWidget(self.fifth_topic)
+
+        self.setWidget(self.widget)
+
+        # Add Scroll options
+        self.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOn)
+        self.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        self.setWidgetResizable(True)
+
+
+
+
+
+
+
+
+
+
+
