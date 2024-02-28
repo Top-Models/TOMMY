1d6ab7117a1293f4ab7ada5cd1e753dfe8cece8a
Belle-and-the-Wolf <91776815+Belle-and-the-Wolf@users.noreply.github.com>
2024-02-28 09:32:49 +0100

    [FEAT] Stopwords display is now a scroll area

--- interactive_topic_modeling/display/stopwords_display.py
+++ interactive_topic_modeling/display/stopwords_display.py
@@ -1,5 +1,5 @@
 from PySide6.QtCore import Qt
-from PySide6.QtWidgets import QGridLayout, QLabel, QScrollArea
+from PySide6.QtWidgets import QGridLayout, QLabel, QScrollArea, QWidget
 
 
 class StopwordsDisplay(QScrollArea):
@@ -10,14 +10,24 @@ class StopwordsDisplay(QScrollArea):
         # Initialize widget properties
         self.setStyleSheet("background-color: white;")
 
-        # Initialize layout
-        self.layout = QGridLayout()
-        self.setLayout(self.layout)
+        # Initialize layout for scroll area
+        self.scroll_area = QWidget()
+        self.layout = QGridLayout(self.scroll_area)
+        self.layout.setAlignment(Qt.AlignCenter)
 
-        # Initialize widgets
-        self.label = QLabel("Stopwords Display")
-        self.label.setAlignment(Qt.AlignCenter)
+        # Initialize excluded words
+        for i in range(1, 5):
+            test_label = QLabel("Word")
+            test_label.setStyleSheet("background-color: pink;")
+            test_label.setAlignment(Qt.AlignCenter)
+            test_label.setFixedSize(200, 100)
+            self.layout.addWidget(test_label)
 
-        # Add widgets
-        self.layout.addWidget(self.label, 0, 0)
+        # Set scroll area as focal point
+        self.setWidget(self.scroll_area)
+
+        # Add scroll options
+        self.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOn)
+        self.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        self.setWidgetResizable(True)
 
