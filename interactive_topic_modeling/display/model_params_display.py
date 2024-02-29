935996b44d26c3b0c971193fe9ad5dedf68df5eb
Belle-and-the-Wolf <91776815+Belle-and-the-Wolf@users.noreply.github.com>
2024-02-29 12:40:43 +0100

    [FEAT] Added a header

--- interactive_topic_modeling/display/model_params_display.py
+++ interactive_topic_modeling/display/model_params_display.py
@@ -1,6 +1,6 @@
 from PySide6.QtCore import Qt
-from PySide6.QtWidgets import QVBoxLayout, QLabel, QScrollArea, QLineEdit, QWidget
-
+from PySide6.QtWidgets import QVBoxLayout, QLabel, QScrollArea, QLineEdit, QWidget, QHBoxLayout
+from interactive_topic_modeling.support.constant_variables import text_font, heading_font
 
 class ModelParamsDisplay(QScrollArea):
 
@@ -22,19 +22,34 @@ class ModelParamsDisplay(QScrollArea):
         self.container_layout = QVBoxLayout(self.container)
         self.container_layout.setAlignment(Qt.AlignTop)
 
-        # Initialize widgets
+        # Add header label
+        header_label = QLabel("Settings")
+        header_label.setStyleSheet(f"font-size: 16px;"
+                                   f"font-family: {heading_font};"
+                                   f"background-color: #E40046;"
+                                   f"color: white;")
+        header_label.setFixedSize(242, 35)
+        header_label.setAlignment(Qt.AlignmentFlag.AlignTop)
+        self.layout.addWidget(header_label)
+        # Initialize topic widgets
         topic_label = QLabel("Number of topics:")
-        topic_label.setAlignment(Qt.AlignCenter)
+        topic_label.setStyleSheet(f"font-size: 16px;"
+                                  f"font-family: {text_font}")
+        topic_label.setAlignment(Qt.AlignmentFlag.AlignLeft)
         self.container_layout.addWidget(topic_label)
 
         self.topic_input = QLineEdit()
         self.topic_input.setPlaceholderText("1")
-        self.topic_input.setStyleSheet("border: 2px solid #E40046;")
-        self.topic_input.setFixedWidth(208)
-        self.topic_input.setAlignment(Qt.AlignCenter)
+        self.topic_input.setStyleSheet(f"border-radius: 5px;"
+                                       f"font-size: 14px;"
+                                       f"font-family: {text_font};"
+                                       f"color: black;"
+                                       f"border: 2px solid #00968F")
+        self.topic_input.setAlignment(Qt.AlignLeft)
         self.container_layout.addWidget(self.topic_input)
 
         # Add widgets
         self.layout.addWidget(self.container)
         self.setStyleSheet("border-bottom: 2px solid #E40046;")
+
 
