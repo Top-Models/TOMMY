9ed37e50f4a8d2b0120337e17aa5264d0b8dff5b
Nick Jordan <nick@jorcademy.nl>
2024-03-01 11:23:35 +0100

    [FIX] File stats display update now works properly when switching file labels.

--- interactive_topic_modeling/display/imported_files_display/file_stats_display.py
+++ interactive_topic_modeling/display/imported_files_display/file_stats_display.py
@@ -1,5 +1,5 @@
 from PySide6.QtCore import Qt
-from PySide6.QtWidgets import QLabel, QScrollArea, QVBoxLayout
+from PySide6.QtWidgets import QLabel, QScrollArea, QVBoxLayout, QLayout
 
 from interactive_topic_modeling.backend.file_import.file import File
 from interactive_topic_modeling.support.constant_variables import seco_col_blue, heading_font
@@ -15,13 +15,7 @@ class FileStatsDisplay(QScrollArea):
 
         # Initialize layout
         self.layout = QVBoxLayout()
-        self.layout.setContentsMargins(20, 20, 0, 0)
         self.setLayout(self.layout)
-
-        # Title widget
-        title_label = QLabel("File stats")
-        title_label.setStyleSheet("font-size: 20px;")
-        title_label.setAlignment(Qt.AlignmentFlag.AlignLeft | Qt.AlignmentFlag.AlignTop)
 
         # Initialize widgets
         self.display_no_file_selected()
@@ -31,30 +25,62 @@ class FileStatsDisplay(QScrollArea):
         Display a message when no file is selected
         :return: None
         """
+
+        # Prepare layout
+        self.clear_layout()
+        self.layout.setContentsMargins(0, 0, 0, 0)
+
+        # Add label
         no_file_selected_label = QLabel("No file selected")
         no_file_selected_label.setStyleSheet("font-size: 20px;")
         no_file_selected_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
         self.layout.addWidget(no_file_selected_label)
 
-    def clear_screen(self):
+    def clear_layout(self) -> None:
         """
-        Clear the screen
+        Clear the layout
         :return: None
         """
-        for i in reversed(range(self.layout.count())):
-            self.layout.itemAt(i).widget().deleteLater()
+        while self.layout.count() > 0:
+            item = self.layout.takeAt(0)
+            if item:
+                layout = item.layout()
+                if layout:
+                    self.clear_sub_layout(layout)
+            widget = item.widget()
+            if widget:
+                widget.deleteLater()
 
-    def display_file_info(self, file: File):
+    def clear_sub_layout(self, layout: QLayout) -> None:
+        """
+        Clear a sub-layout
+        :param layout: The sub-layout to clear
+        :return: None
+        """
+        while layout.count() > 0:
+            item = layout.takeAt(0)
+            if item:
+                widget = item.widget()
+                if widget:
+                    widget.deleteLater()
+                else:
+                    self.clear_sub_layout(item.layout())
+
+    def display_file_info(self, file: File) -> None:
         """
         Display the file info
         :param file: The file to display
         :return: None
         """
-        self.clear_screen()
+
+        # Prepare layout
+        self.clear_layout()
 
         # Use a vertical layout
         vertical_layout = QVBoxLayout()
         vertical_layout.setAlignment(Qt.AlignmentFlag.AlignTop | Qt.AlignmentFlag.AlignLeft)
+        vertical_layout.setContentsMargins(20, 20, 0, 0)  # Adjust the left margin here
+        vertical_layout.setSpacing(10)
         self.layout.addLayout(vertical_layout)
 
         # Add file name
@@ -63,12 +89,18 @@ class FileStatsDisplay(QScrollArea):
                                       f"font-family: {heading_font};"
                                       f"font-weight: bold;"
                                       f"text-transform: uppercase;")
+        file_name_label.setAlignment(Qt.AlignmentFlag.AlignLeft | Qt.AlignmentFlag.AlignTop)
         vertical_layout.addWidget(file_name_label)
 
         # Add file format
         file_format_label = QLabel(f"Format: {file.format}")
         file_format_label.setStyleSheet("font-size: 16px;"
                                         "margin-top: 10px;")
+        file_format_label.setAlignment(Qt.AlignmentFlag.AlignLeft | Qt.AlignmentFlag.AlignTop)
         vertical_layout.addWidget(file_format_label)
+
+        print(self.layout.count())
+
+
 
 
