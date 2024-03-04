cf9c6661f14ac7ed1f201b235ee80a79622920ed
Thochril <tcloef@gmail.com>
2024-03-04 10:48:24 +0100

    Add basic common words graphs

--- interactive_topic_modeling/display/graph_display.py
+++ interactive_topic_modeling/display/graph_display.py
@@ -33,7 +33,7 @@ def perform_lda_on_text(text, num_topics):
 def generate_list():
     # Define the range of numbers
     low_range = 1
-    high_range = 1500
+    high_range = 10050
 
     # Define the desired length of the list
     list_length = 1000
@@ -131,9 +131,9 @@ In een wereld vol chaos en onzekerheid herinneren panda's ons eraan om te vertra
 
     def add_lda_plots(self, tab_name: str, lda_model) -> None:
         """
-        Add a word cloud plot for the given LDA model
-        :param tab_name: Name of the tab to add the plot to
-        :param lda_model: The LDA model to add a plot for
+        Add word cloud plots for the given LDA model
+        :param tab_name: Name of the tab to add the plots to
+        :param lda_model: The LDA model to add the plots for
         :return: None
         """
         plots = []
@@ -142,14 +142,16 @@ In een wereld vol chaos en onzekerheid herinneren panda's ons eraan om te vertra
 
         plots.extend(self.construct_wordclouds(tab_name, lda_model))
         plots.append(self.construct_word_count(tab_name))
+        plots.extend(self.construct_common_words(tab_name, lda_model))
 
         for plot in plots:
             self.plots_container[tab_name].append(plot)
 
-    def construct_wordclouds(self, tab_name: str, lda_model):
+    def construct_wordclouds(self, tab_name: str, lda_model) -> list[FigureCanvas]:
         canvases = []
         for topic_id, topic in enumerate(lda_model.print_topics(num_topics=self.num_topics, num_words=20)):
             topic_words = " ".join([word.split("*")[1].strip() for word in topic[1].split(" + ")])
+
             wordcloud = WordCloud(width=800, height=800, random_state=15, max_font_size=110).generate(topic_words)
 
             # Add topics to topic display
@@ -167,18 +169,38 @@ In een wereld vol chaos en onzekerheid herinneren panda's ons eraan om te vertra
             canvases.append(FigureCanvas(fig))
         return canvases
 
-    def construct_word_count(self, tab_name: str):
+
+    def construct_common_words(self, tab_name: str, lda_model) -> list[FigureCanvas]:
+        canvases = []
+        for topic_id, topic in enumerate(lda_model.print_topics(num_topics=self.num_topics, num_words=10)):
+            topic_words = [word.split("*")[1].strip() for word in topic[1].split(" + ")]
+            topic_weights = [float(word.split("*")[0].strip()) for word in topic[1].split(" + ")]
+            print(topic_weights)
+
+            fig = plt.figure()
+            plt.bar(topic_words, topic_weights, color="darkblue")
+
+            plt.margins(0.02)
+            plt.ylabel("gewicht")
+            plt.title("Meest voorkomende woorden topic {}".format(topic_id))
+
+            canvases.append(FigureCanvas(fig))
+
+        return canvases
+
+    def construct_word_count(self, tab_name: str) -> FigureCanvas:
         document_counts = generate_list()
-        max_document_count = 1500
 
         fig = plt.figure()
-        plt.hist(document_counts, bins=max_document_count//10, color="darkblue")
+        plt.hist(document_counts, bins=150, color="darkblue")
 
-        plt.gca().set(xlabel="aantal woorden per document", ylabel="aantal documenten")
-        plt.margins(x=0.01)
+        plt.margins(x=0.02)
+        plt.xlabel("aantal woorden per document")
+        plt.ylabel("aantal documenten")
         plt.title("Distributie aantal woorden per document")
 
         return FigureCanvas(fig)
+
 
 
     def display_plot(self, tab_name: str, plot_index: int) -> None:
