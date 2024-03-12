07a30fbe4c1bcdd8db7178121989284700a1145e
Belle-and-the-Wolf <91776815+Belle-and-the-Wolf@users.noreply.github.com>
2024-03-12 15:49:57 +0100

    [REFACTOR] generic_file_importer.py is now following the styleguide

--- interactive_topic_modeling/backend/file_import/generic_file_importer.py
+++ interactive_topic_modeling/backend/file_import/generic_file_importer.py
@@ -1,14 +1,27 @@
+from typing import List, Generator
+
+from interactive_topic_modeling.backend.file_import.file import File
 from interactive_topic_modeling.backend.file_import import file_importer_base
 from interactive_topic_modeling.backend.file_import import csv_file_importer
-from typing import List, Generator
-from interactive_topic_modeling.backend.file_import.file import File
 
 
 class GenericFileImporter:
+    """
+    The class GenericFileImporter is responsible for importing files using FileImporterBase objects
+    """
     def __init__(self):
+        """
+        Initialization of a new GenericFileImporter object.
+        """
         self.importers: List[file_importer_base.FileImporterBase] = [csv_file_importer.CsvFileImporter()]
 
     def import_file(self, path: str) -> Generator[File, None, None]:
+        """
+        Imports a file from the specified path using compatible importers.
+
+        :param path: The string path of the file to import.
+        :return Generator[File, None, None]: A generator yielding File objects.
+        """
         for importer in self.importers:
             if importer.compatible_file(path):
                 return importer.load_file(path)
