bd14f3471df09a0df21ea120d85a8c706dbf8a69
Erben Klaver <erbenjerbenjk@gmail.com>
2024-03-12 15:28:36 +0100

    added test test

--- test/example_test.py
+++ test/example_test.py
@@ -1,46 +1,25 @@
 import pytest
-from PySide6.QtWidgets import QApplication
 from PySide6.QtCore import Qt
-from interactive_topic_modeling.display.graph_display import GraphDisplay
+from interactive_topic_modeling.display.model_params_display import ModelParamsDisplay
 
 
 @pytest.fixture
-def graph_display():
-    # Create a PySide6 application instance
-    app = QApplication([])
-
-    # Create an instance of GraphDisplay with the correct parent (QWidget)
-    display = GraphDisplay()
-    display.setParent(None)  # Set parent to None or an appropriate QWidget if needed
-
-    app.setParent(None)
-
-    yield display
-
-    # Cleanup
-    app.quit()
+def parameter_display(qtbot):
+    # Create and return your parameter_display object here
+    parameter_display = ModelParamsDisplay()
+    # Attach the widget to the bot that will simulate the user
+    qtbot.addWidget(parameter_display)
+    return parameter_display
 
 
-class TestGraphDisplay:
-    def test_next_plot(self, graph_display):
-        # Perform any necessary setup or trigger events to reach the desired state
-        # For example, clicking on a tab
-        graph_display.setCurrentIndex(0)
-
-        # Get the current tab name
-        tab_name = graph_display.tabText(graph_display.currentIndex())
-
-        # Get the current plot index
-        assert len(graph_display.plot_index.items()) > 0
-        current_plot_index = graph_display.plot_index[tab_name]
-
-        # Trigger the next_plot method
-        graph_display.next_plot(tab_name)
-
-        # Check if the plot index has been updated correctly
-        assert graph_display.plot_index[tab_name] == (current_plot_index + 1) % len(
-            graph_display.plots_container[tab_name])
+def test_model_params_display_user_interaction(parameter_display, qtbot):
+    # Set some kind of input
+    parameter_display.topic_input.setText("10")
+    # Have the user simulate the action of validating the input
+    qtbot.keyPress(parameter_display.topic_input, Qt.Key_Return)
+    # See if the result is the same as expected
+    assert parameter_display.fetch_topic_num() == 10
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     pytest.main()
