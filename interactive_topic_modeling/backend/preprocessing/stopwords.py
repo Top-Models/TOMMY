d9d20a49a9fa092769cfd4074b4859185f101ec1
Belle-and-the-Wolf <91776815+Belle-and-the-Wolf@users.noreply.github.com>
2024-03-13 10:08:27 +0100

    [REFACTOR] stopwords.py and tokenizer.py are now following the styleguide

--- interactive_topic_modeling/backend/preprocessing/stopwords.py
+++ interactive_topic_modeling/backend/preprocessing/stopwords.py
@@ -2,23 +2,34 @@ from collections.abc import Iterable
 
 
 class StopWords:
+    """A class representing the sets of stopwords."""
 
     def __init__(self, words: Iterable[str]) -> None:
+        """Initialize the Stopwords object."""
         self.words = set(words)
 
     def __str__(self) -> str:
+        """Get a sting representation of the stop words"""
         return f"{list(self.words)}"
 
     def __len__(self) -> int:
+        """Get the number of stop words"""
         return len(self.words)
 
     def __contains__(self, word: str) -> bool:
+        """Check if the stop words list contains a word"""
         return word in self.words
 
     def __iter__(self) -> Iterable[str]:
+        """Return an iterable stopword list"""
         return iter(self.words)
 
     def add(self, *args: str | Iterable[str]) -> None:
+        """
+        Add one or more stop words
+        :param args: The word(s) to add to the iterable.
+        :return: None.
+        """
         # Only 1 argument and it's a list or tuple
         if len(args) == 1 and isinstance(args[0], Iterable):
             words = args[0]
@@ -30,6 +41,11 @@ class StopWords:
             self.words.add(word)
 
     def remove(self, *args: str | Iterable[str]) -> None:
+        """
+        Remove one or more stop words
+        :param args: The word(s) to remove from the iterable.
+        :return: None.
+        """
         # Only 1 argument and it's a list or tuple
         if len(args) == 1 and isinstance(args[0], Iterable):
             words = args[0]
--- interactive_topic_modeling/backend/preprocessing/tokenizer.py
+++ interactive_topic_modeling/backend/preprocessing/tokenizer.py
@@ -4,14 +4,35 @@ from spacy import Language
 
 
 class NLTKTokenizer:
+    """
+    class for tokenizing text using NLTk;s RegexpTokenizer.
+
+    It uses:
+    _vocab (spacy.vocab.Vocab): The vocabulary object from a spaCy
+                                language pipeline.
+    _tokenizer (nltk.tokenize.RegexpTokenizer): The tokenizer object
+                                                from NLTK.
+    """
     def __init__(self, nlp: Language) -> None:
+        """
+        Initialize the NLTKTokenizer object.
+
+        :param nlp: The spaCy language object.
+        :return: None.
+        """
         self._vocab = nlp.vocab
         self._tokenizer = RegexpTokenizer(r'\w+')
 
     def __call__(self, doc: str) -> Doc:
+        """
+        Tokenize a document.
+        :param doc: The input document to be tokenized.
+        :return Doc: The tokenized document as a spaCy Doc object.
+        """
         doc = doc.lower()
         words = self._tokenizer.tokenize(doc)
-        words = [word for word in words if (not word.isnumeric()) and (len(word) > 1)]
+        words = [word for word in words if (not word.isnumeric())
+                 and (len(word) > 1)]
         spaces = [True] * len(words)
 
         return Doc(self._vocab, words=words, spaces=spaces)
