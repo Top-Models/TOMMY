bb066918ee51433b0a6ede87c5198ee544b34a3d
Belle-and-the-Wolf <91776815+Belle-and-the-Wolf@users.noreply.github.com>
2024-03-13 10:31:52 +0100

    [REFACTOR] file_stats_display.py is now following the styleguide

--- interactive_topic_modeling/display/imported_files_display/file_stats_display.py
+++ interactive_topic_modeling/display/imported_files_display/file_stats_display.py
@@ -2,12 +2,17 @@ from PySide6.QtCore import Qt
 from PySide6.QtWidgets import QLabel, QScrollArea, QVBoxLayout, QLayout
 
 from interactive_topic_modeling.backend.file_import.file import File
-from interactive_topic_modeling.support.constant_variables import seco_col_blue, heading_font, hover_seco_col_blue
+from interactive_topic_modeling.support.constant_variables import (
+    seco_col_blue,
+    heading_font,
+    hover_seco_col_blue)
 
 
 class FileStatsDisplay(QScrollArea):
+    """Class to define the FileStatsDisplay UI component"""
 
-    def __init__(self):
+    def __init__(self) -> None:
+        """Initialize the FileStatsDisplay."""
         super().__init__()
 
         # Initialize widget properties
@@ -25,7 +30,7 @@ class FileStatsDisplay(QScrollArea):
         # Initialize widgets
         self.display_no_file_selected()
 
-    def add_title_widget(self):
+    def add_title_widget(self) -> None:
         """
         Add the title label widget
         """
@@ -36,18 +41,20 @@ class FileStatsDisplay(QScrollArea):
                                   f"text-transform: uppercase;"
                                   f"background-color: {seco_col_blue};"
                                   f"color: white;"
-                                  f"border-bottom: 3px solid {hover_seco_col_blue};")
+                                  f"border-bottom: "
+                                  f"3px solid {hover_seco_col_blue};")
+
         title_label.setContentsMargins(0, 0, 0, 0)
         title_label.setMaximumHeight(50)
-        title_label.setAlignment(Qt.AlignmentFlag.AlignCenter | Qt.AlignmentFlag.AlignTop)
+        title_label.setAlignment(Qt.AlignmentFlag.AlignCenter |
+                                 Qt.AlignmentFlag.AlignTop)
         self.layout.addWidget(title_label)
 
     def display_no_file_selected(self) -> None:
         """
-        Display a message when no file is selected
+        Display a message when no file is selected.
         :return: None
         """
-
         # Prepare layout
         self.clear_layout()
         self.layout.setContentsMargins(0, 0, 0, 0)
@@ -60,7 +67,7 @@ class FileStatsDisplay(QScrollArea):
 
     def clear_layout(self) -> None:
         """
-        Clear the layout
+        Clear the layout.
         :return: None
         """
         while self.layout.count() > 1:
@@ -100,8 +107,11 @@ class FileStatsDisplay(QScrollArea):
 
         # Use a vertical layout
         vertical_layout = QVBoxLayout()
-        vertical_layout.setAlignment(Qt.AlignmentFlag.AlignTop | Qt.AlignmentFlag.AlignLeft)
-        vertical_layout.setContentsMargins(20, 20, 0, 0)  # Adjust the left margin here
+        vertical_layout.setAlignment(Qt.AlignmentFlag.AlignTop |
+                                     Qt.AlignmentFlag.AlignLeft)
+
+        # Adjust the left margin here
+        vertical_layout.setContentsMargins(20, 20, 0, 0)
         vertical_layout.setSpacing(10)
         self.layout.addLayout(vertical_layout)
 
@@ -111,29 +121,34 @@ class FileStatsDisplay(QScrollArea):
                                       f"font-family: {heading_font};"
                                       f"font-weight: bold;"
                                       f"text-transform: uppercase;")
-        file_name_label.setAlignment(Qt.AlignmentFlag.AlignLeft | Qt.AlignmentFlag.AlignTop)
+        file_name_label.setAlignment(Qt.AlignmentFlag.AlignLeft |
+                                     Qt.AlignmentFlag.AlignTop)
         vertical_layout.addWidget(file_name_label)
 
         # Add file path
         file_path_label = QLabel(f"Pad: {file.path}")
         file_path_label.setStyleSheet("font-size: 16px;")
-        file_path_label.setAlignment(Qt.AlignmentFlag.AlignLeft | Qt.AlignmentFlag.AlignTop)
+        file_path_label.setAlignment(Qt.AlignmentFlag.AlignLeft |
+                                     Qt.AlignmentFlag.AlignTop)
         vertical_layout.addWidget(file_path_label)
 
         # Add file format
         file_format_label = QLabel(f"Formaat: {file.format}")
         file_format_label.setStyleSheet("font-size: 16px;")
-        file_format_label.setAlignment(Qt.AlignmentFlag.AlignLeft | Qt.AlignmentFlag.AlignTop)
+        file_format_label.setAlignment(Qt.AlignmentFlag.AlignLeft |
+                                       Qt.AlignmentFlag.AlignTop)
         vertical_layout.addWidget(file_format_label)
 
         # Add word amount
         word_amount_label = QLabel(f"Aantal woorden: {file.length}")
         word_amount_label.setStyleSheet("font-size: 16px;")
-        word_amount_label.setAlignment(Qt.AlignmentFlag.AlignLeft | Qt.AlignmentFlag.AlignTop)
+        word_amount_label.setAlignment(Qt.AlignmentFlag.AlignLeft |
+                                       Qt.AlignmentFlag.AlignTop)
         vertical_layout.addWidget(word_amount_label)
 
         # Add file size
         file_size_label = QLabel(f"Grootte: {file.size}B")
         file_size_label.setStyleSheet("font-size: 16px;")
-        file_size_label.setAlignment(Qt.AlignmentFlag.AlignLeft | Qt.AlignmentFlag.AlignTop)
+        file_size_label.setAlignment(Qt.AlignmentFlag.AlignLeft |
+                                     Qt.AlignmentFlag.AlignTop)
         vertical_layout.addWidget(file_size_label)
