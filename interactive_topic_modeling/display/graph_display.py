973f492ada7565a086898cdfca963fc40a7dd4b3
Thochril <tcloef@gmail.com>
2024-03-25 09:03:40 +0100

    Experiment with graph visualization techniques

--- interactive_topic_modeling/display/graph_display.py
+++ interactive_topic_modeling/display/graph_display.py
@@ -168,9 +168,11 @@ class GraphDisplay(QTabWidget):
         canvases = []
 
         for i in range(self.num_topics):
-            wordcloud = WordCloud(width=800, height=400, background_color='white').generate_from_frequencies(
-                dict(lda_model.show_topic(i, 30))
-            )
+            wordcloud = WordCloud(
+                width=800,
+                height=400,
+                background_color='white'
+            ).generate_from_frequencies(dict(lda_model.show_topic(i, 30)))
 
             # Construct a word cloud
             fig = plt.figure()
@@ -182,9 +184,11 @@ class GraphDisplay(QTabWidget):
 
         return canvases
 
-    def construct_probable_words(self, lda_model: GensimLdaModel) -> list[FigureCanvas]:
+    def construct_probable_words(self, lda_model: GensimLdaModel) \
+            -> list[FigureCanvas]:
         """
-        Construct bar plots for the words with the highest probability for the given LDA model
+        Construct bar plots for the words with the highest probability for the
+        given LDA model
         :param lda_model: The LDA model to construct the plots for
         :return: A list of probable words plots
         """
@@ -208,10 +212,12 @@ class GraphDisplay(QTabWidget):
 
     def construct_word_count(self) -> FigureCanvas:
         """
-        Construct a histogram containing word counts of each input document for the given LDA model
+        Construct a histogram containing word counts of each input document for
+        the given LDA model
         :return: A word count plot
         """
-        document_counts = [1, 2, 3, 4, 6, 7, 4, 6, 2, 9, 8, 3, 3, 6, 7, 4, 6, 2, 9, 8, 3, 3, 6]
+        document_counts = [1, 2, 3, 4, 6, 7, 4, 6, 2, 9, 8, 3, 3, 6, 7, 4, 6,
+                           2, 9, 8, 3, 3, 6]
 
         # Construct a histogram
         fig = plt.figure()
@@ -225,9 +231,11 @@ class GraphDisplay(QTabWidget):
 
         return FigureCanvas(fig)
 
-    def construct_correlation_matrix(self, lda_model: GensimLdaModel) -> FigureCanvas:
+    def construct_correlation_matrix(self, lda_model: GensimLdaModel) \
+            -> FigureCanvas:
         """
-        Construct a correlation matrix plot comparing the different topics with each other for the given LDA model
+        Construct a correlation matrix plot comparing the different topics with
+        each other for the given LDA model
         :param lda_model: The LDA model to construct the plot for
         :return: A correlation matrix plot
         """
@@ -250,9 +258,11 @@ class GraphDisplay(QTabWidget):
 
         return FigureCanvas(fig)
 
-    def construct_word_topic_network_vis(self, lda_model: GensimLdaModel) -> FigureCanvas:
+    def construct_word_topic_network_vis(self, lda_model: GensimLdaModel) \
+            -> FigureCanvas:
         """
-        Construct a word-topic network plot showing the relations between topics and probable words
+        Construct a word-topic network plot showing the relations between
+        topics and probable words
         :param lda_model: The LDA model to construct the plot for
         :return: A word-topic network plot
         """
@@ -269,12 +279,15 @@ class GraphDisplay(QTabWidget):
 
         # Get drawing function arguments
         node_sizes = [150 if node[1] is not None else 0 for node in nodes]
-        node_colors = [node[1] if node[1] is not None else "black" for node in nodes]
+        node_colors = [node[1] if node[1] is not None else "black"
+                       for node in nodes]
 
         edge_colors = [graph[u][v]["color"] for (u, v) in edges]
-        edge_width = [(graph[u][v]["weight"] * edge_scale_factor) for u, v in edges]
+        edge_width = [(graph[u][v]["weight"] * edge_scale_factor)
+                      for u, v in edges]
 
-        # Draw the network using the kamada-kawai algorithm to position the nodes in an aesthetically pleasing way
+        # Draw the network using the kamada-kawai algorithm to position the
+        # nodes in an aesthetically pleasing way
         nx.draw_kamada_kawai(graph,
                              node_size=node_sizes,
                              with_labels=True,
@@ -285,18 +298,22 @@ class GraphDisplay(QTabWidget):
 
         return FigureCanvas(fig)
 
-    def construct_word_topic_network(self, lda_model: GensimLdaModel) -> nx.Graph:
+    def construct_word_topic_network(self, lda_model: GensimLdaModel) \
+            -> nx.Graph:
         """"
-        Construct a word-topic network which is used to plot the relations between topics and probable words
+        Construct a word-topic network which is used to plot the relations
+        between topics and probable words
         :param lda_model: The LDA model to construct the network for
         :return: A networkx graph
         """
         graph = nx.Graph()
 
-        # List of simple, distinct colors from https://sashamaps.net/docs/resources/20-colors/
-        colors = ['#e6194b', '#3cb44b', '#ffe119', '#4363d8', '#f58231', '#9a6324', '#46f0f0', '#f032e6', '#bcf60c',
-                  '#fabebe', '#008080', '#e6beff', '#000075', '#fffac8', '#800000', '#aaffc3', '#808000', '#ffd8b1',
-                  '#808080', '#911eb4']
+        # List of simple, distinct colors from
+        # https://sashamaps.net/docs/resources/20-colors/
+        colors = ['#e6194b', '#3cb44b', '#ffe119', '#4363d8', '#f58231',
+                  '#9a6324', '#46f0f0', '#f032e6', '#bcf60c', '#fabebe',
+                  '#008080', '#e6beff', '#000075', '#fffac8', '#800000',
+                  '#aaffc3', '#808000', '#ffd8b1', '#808080', '#911eb4']
 
         # Amount of words displayed for each topic
         node_amount = 15
@@ -305,13 +322,18 @@ class GraphDisplay(QTabWidget):
             topic_tuples = lda_model.show_topic(topic_id, node_amount)
             for topic_tuple in topic_tuples:
                 graph.add_node(topic_id, color=colors[topic_id%20])
-                graph.add_edge(topic_id, topic_tuple[0], color=colors[topic_id%20], weight=topic_tuple[1])
+                graph.add_edge(
+                    topic_id,
+                    topic_tuple[0],
+                    color=colors[topic_id%20],
+                    weight=topic_tuple[1])
+
         return graph
 
     def get_edge_scale_factor(self, lda_model: GensimLdaModel) -> float:
         """
-        Calculates the scale factor to make sure the biggest edge in a network is always the same size, regardless of
-        the maximum topic weight
+        Calculates the scale factor to make sure the biggest edge in a network
+        is always the same size, regardless of the maximum topic weight
         :param lda_model: The LDA model to calculate the scale factor for
         :return: The edge scale factor
         """
@@ -322,21 +344,24 @@ class GraphDisplay(QTabWidget):
             _, topic_weights = lda_model.show_topic_and_probs(topic_id, 1)
             max_topic_weight = max(max_topic_weight, topic_weights[0])
 
-        # A constant which is multiplied by the scale factor according to an edge width that is visually pleasing
+        # A constant which is multiplied by the scale factor according to an
+        # edge width that is visually pleasing
         chosen_weight = 1.5
 
         scale_factor = (1/max_topic_weight)
 
         return scale_factor*chosen_weight
 
-    def construct_doc_topic_network_vis(self, lda_model: GensimLdaModel) -> FigureCanvas:
+    def construct_doc_topic_network_vis(self, lda_model: GensimLdaModel) \
+            -> FigureCanvas:
         fig = plt.figure()
         graph = self.construct_doc_topic_network(lda_model)
         edges = graph.edges()
         nodes = graph.nodes(data="color")
 
         node_sizes = [150 if node[1] is not None else 8 for node in nodes]
-        node_colors = [node[1] if node[1] is not None else "black" for node in nodes]
+        node_colors = [node[1] if node[1] is not None else "black"
+                       for node in nodes]
 
         edge_colors = [graph[u][v]["color"] for (u, v) in edges]
         edge_width = [(graph[u][v]["weight"]) for u, v in edges]
@@ -349,21 +374,26 @@ class GraphDisplay(QTabWidget):
 
         return FigureCanvas(fig)
 
-    def construct_doc_topic_network(self, lda_model: GensimLdaModel) -> nx.Graph:
+    def construct_doc_topic_network(self, lda_model: GensimLdaModel) \
+            -> nx.Graph:
         graph = nx.Graph()
 
-        # List of simple, distinct colors from https://sashamaps.net/docs/resources/20-colors/
-        colors = ['#e6194b', '#3cb44b', '#ffe119', '#4363d8', '#f58231', '#9a6324', '#46f0f0', '#f032e6', '#bcf60c',
-                  '#fabebe', '#008080', '#e6beff', '#000075', '#fffac8', '#800000', '#aaffc3', '#808000', '#ffd8b1',
-                  '#808080', '#911eb4']
+        # List of simple, distinct colors from
+        # https://sashamaps.net/docs/resources/20-colors/
+        colors = ['#e6194b', '#3cb44b', '#ffe119', '#4363d8', '#f58231',
+                  '#9a6324', '#46f0f0', '#f032e6', '#bcf60c', '#fabebe',
+                  '#008080', '#e6beff', '#000075', '#fffac8', '#800000',
+                  '#aaffc3', '#808000', '#ffd8b1', '#808080', '#911eb4']
 
         for topic_id in range(self.num_topics):
             graph.add_node(topic_id, color=colors[topic_id%20])
 
+
+        # TODO get accurate data instead of lda_model.bags_of_words
         for document_id in range(len(lda_model.bags_of_words)):
             document_topic = (lda_model.get_document_topics(
                               lda_model.bags_of_words[document_id],
-             0.02))
+             0.25))
             for (topic_id, topic_probability) in document_topic:
                 graph.add_edge(topic_id,
                                document_id,
@@ -371,10 +401,12 @@ class GraphDisplay(QTabWidget):
                                weight=topic_probability)
 
         # for topic_id in range(self.num_topics):
-        #     topic_tuples = lda_model.show_topic(topic_id, node_amount)
-        #     for topic_tuple in topic_tuples:
-        #         graph.add_node(topic_id, color=colors[topic_id%20])
-        #         graph.add_edge(topic_id, topic_tuple[0], color=colors[topic_id%20], weight=topic_tuple[1])
+        #     for neighbor in graph[topic_id]:
+        #         if graph.degree[neighbor] < 2
+
+        nodes_to_be_removed = [node for node,degree in graph.degree()
+                               if degree < 2]
+        graph.remove_nodes_from(nodes_to_be_removed)
 
         return graph
 
