f28a0de77b1633255934268e2b08d78a510da791
Belle-and-the-Wolf <91776815+Belle-and-the-Wolf@users.noreply.github.com>
2024-04-08 07:54:49 +0200

    Fixed the highlighting of words to work again

--- tommy/view/topic_view/fetched_topics_view.py
+++ tommy/view/topic_view/fetched_topics_view.py
@@ -67,7 +67,7 @@ class FetchedTopicsView(QScrollArea, Observer):
 
         # Add topic to display
         topic_entity = TopicEntity(topic_name, topic_words)
-        # topic_entity.wordClicked.connect(self.on_word_clicked)
+        topic_entity.wordClicked.connect(self.on_word_clicked)
         self.layout.addWidget(topic_entity)
 
     def display_topics(self, tab_name: str) -> None:
@@ -88,7 +88,7 @@ class FetchedTopicsView(QScrollArea, Observer):
         # Add topics to view
         for topic_name, topic_words in self.topic_container[tab_name]:
             topic_entity = TopicEntity(topic_name, topic_words)
-            # topic_entity.wordClicked.connect(self.on_word_clicked)
+            topic_entity.wordClicked.connect(self.on_word_clicked)
             self.layout.addWidget(topic_entity)
 
     def remove_tab_from_container(self, tab_name: str) -> None:
