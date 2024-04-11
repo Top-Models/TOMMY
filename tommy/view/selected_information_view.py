0f80eadaa8c3b87df95f4ae804ed655ca3f62e5c
Mees <m.i.j.notermans@students.uu.nl>
2024-04-11 14:26:28 +0000

    Renamed FileStatsView to SelectedInformationView.

--- tommy/view/selected_information_view.py
+++ tommy/view/selected_information_view.py
@@ -13,7 +13,7 @@ from tommy.view.observer.observer import Observer
 
 
 class SelectedInformationView(QScrollArea, Observer):
-    """Class to define the FileStatsDisplay UI component"""
+    """Class to define the SelectedInformationView UI component"""
 
     def __init__(self,
                  graph_controller: GraphController,
