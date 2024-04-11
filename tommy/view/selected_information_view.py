4583df54222f9401790a8ce4b49b4a139c3f4702
Mees <m.i.j.notermans@students.uu.nl>
2024-04-11 14:26:01 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- tommy/view/selected_information_view.py
+++ tommy/view/selected_information_view.py
@@ -83,7 +83,7 @@ class SelectedInformationView(QScrollArea, Observer):
 
     def display_no_component_selected(self) -> None:
         """
-        Display a message when no file is selected.
+        Display a message when no component is selected.
         :return: None
         """
         # Prepare layout
