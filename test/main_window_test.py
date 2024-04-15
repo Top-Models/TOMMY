793f4791676c6bc8995646c031b7cc562f87a2c5
Nick Jordan <nick@jorcademy.nl>
2024-04-15 11:59:45 +0200

    Tested SelectedInformationView. Changed name of main_window_test file. Removed commented tests from the main_window_test file.

---
+++ test/main_window_test.py
@@ -0,0 +1,14 @@
+import pytest
+from PySide6 import QtCore
+from PySide6.QtCore import Qt
+from tommy.main_window import MainWindow
+from tommy.support.constant_variables import (text_font, heading_font,
+                                              seco_col_blue,
+                                              hover_seco_col_blue)
+
+"""
+This program has been developed by students from the bachelor Computer Science
+at Utrecht University within the Software Project course.
+© Copyright Utrecht University 
+(Department of Information and Computing Sciences)
+"""
