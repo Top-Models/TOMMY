f845f2db76f250a19b5c79db88bd7ea66f14158f
Nick Jordan <nick@jorcademy.nl>
2024-04-16 12:57:34 +0200

    Tested event handlers in MainWindow.

--- test/main_window_test.py
+++ test/main_window_test.py
@@ -1,10 +1,68 @@
 import pytest
-from PySide6 import QtCore
-from PySide6.QtCore import Qt
+
+from tommy.controller.file_import.metadata import Metadata
 from tommy.main_window import MainWindow
-from tommy.support.constant_variables import (text_font, heading_font,
-                                              seco_col_blue,
-                                              hover_seco_col_blue)
+from tommy.view.imported_files_view.file_label import FileLabel
+
+
+@pytest.fixture(scope='function')
+def main_window(qtbot):
+    main_window = MainWindow()
+    qtbot.addWidget(main_window)
+    return main_window
+
+
+def test_on_file_clicked_selected_file(main_window, mocker):
+    # Arrange
+    metadata = Metadata(name='test', size=1, length=1, format='test')
+    file_label = FileLabel(metadata)
+    file_label.selected = True
+    mocked_fetched_topics_view = mocker.patch.object(
+        main_window.fetched_topics_view, 'deselect_all_topics')
+    mocked_selected_information_view = mocker.patch.object(
+        main_window.selected_information_view, 'display_file_info')
+
+    # Act
+    main_window.on_file_clicked(file_label)
+
+    # Assert
+    mocked_fetched_topics_view.assert_called_once_with()
+    mocked_selected_information_view.assert_called_once_with(file_label)
+
+
+def test_on_file_clicked_file_not_selected(main_window, mocker):
+    # Arrange
+    metadata = Metadata(name='test', size=1, length=1, format='test')
+    file_label = FileLabel(metadata)
+    file_label.selected = False
+    mocked_fetched_topics_view = mocker.patch.object(
+        main_window.fetched_topics_view, 'deselect_all_topics')
+    mocked_selected_information_view = mocker.patch.object(
+        main_window.selected_information_view, 'display_run_info')
+
+    # Act
+    main_window.on_file_clicked(file_label)
+
+    # Assert
+    mocked_fetched_topics_view.assert_called_once_with()
+    mocked_selected_information_view.assert_called_once_with("lda_model")
+
+
+def test_on_topic_clicked(main_window, mocker):
+    # Arrange
+    topic_entity = mocker.Mock()
+    mocked_imported_files_view = mocker.patch.object(
+        main_window.imported_files_view, 'deselect_all_files')
+    mocked_selected_information_view = mocker.patch.object(
+        main_window.selected_information_view, 'display_topic_info')
+
+    # Act
+    main_window.on_topic_clicked(topic_entity)
+
+    # Assert
+    mocked_imported_files_view.assert_called_once_with()
+    mocked_selected_information_view.assert_called_once_with(topic_entity)
+
 
 """
 This program has been developed by students from the bachelor Computer Science
