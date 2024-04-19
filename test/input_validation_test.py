a2fb192e09195cc4d7f225da80f7973b5264873b
Mees <meesoke@hotmail.com>
2024-04-19 17:54:00 +0200

    Added more tests for input validation

--- test/input_validation_test.py
+++ test/input_validation_test.py
@@ -1,9 +1,9 @@
+from unittest import mock
+
 import pytest
 from pytestqt.qtbot import QtBot
 
 from tommy.controller.controller import Controller
-from tommy.controller.model_parameters_controller import \
-    ModelParametersController
 from tommy.view.model_params_view import ModelParamsView
 
 
@@ -11,7 +11,7 @@ from tommy.view.model_params_view import ModelParamsView
 def model_params_view(qtbot: QtBot):
     controller = Controller()
     mpv = ModelParamsView(controller.model_parameters_controller, controller)
-    qtbot.addWidget(mpv)
+    # qtbot.addWidget(mpv)
     return mpv
 
 
@@ -19,10 +19,36 @@ def model_params_view(qtbot: QtBot):
                          [("0", False), ("abc", False), ("1", True),
                           ("2.5", False), ("7", True), ("999", True),
                           ("1000", False)])
-def test_validate_input_return_value(model_params_view: ModelParamsView,
-                                     qtbot: QtBot, test_input, expected):
+def test_validate_input(model_params_view: ModelParamsView, test_input: str,
+                        expected: bool):
     model_params_view.topic_input.setText(test_input)
-    assert model_params_view.validate_input() is expected
+    assert model_params_view.validate_input() == expected
+
+
+@pytest.mark.parametrize("test_input,expected",
+                         [("0", 0), ("abc", 0), ("1", 1),
+                          ("2.5", 0), ("7", 7), ("999", 999),
+                          ("1000", 0)])
+def test_fetch_topic_num(model_params_view: ModelParamsView, test_input: str,
+                         expected: int):
+    model_params_view.topic_input.setText(test_input)
+    assert model_params_view.fetch_topic_num() == expected
+
+
+def test_apply_validates_input(model_params_view: ModelParamsView,
+                               mocker: mock):
+    # test if validate_input is called by apply_button_clicked_event
+    method_spy = mocker.spy(model_params_view, "validate_input")
+    model_params_view.apply_button_clicked_event()
+    method_spy.assert_called_once()
+
+
+def test_editing_finished_validates_input(model_params_view: ModelParamsView,
+                                          mocker: mock):
+    # test if validate_input is called by topic_input_editing_finished_event
+    method_spy = mocker.patch.object(model_params_view, "validate_input")
+    model_params_view.topic_input_editing_finished_event()
+    method_spy.assert_called_once()
 
 
 """
