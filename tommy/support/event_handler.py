383378cde09173e391ac3be440f48fdc70d53892
TemporarilyTired <66777276+TemporarilyTired@users.noreply.github.com>
2024-04-24 14:33:43 +0200

    Fixes from merging main into eventhandler: remove obsolete observer usage Add those copyright docstrings

--- tommy/support/event_handler.py
+++ tommy/support/event_handler.py
@@ -33,3 +33,11 @@ class EventHandler[T]:
         """
         for callback in self.subscribers:
             callback(event_data)
+
+
+"""
+This program has been developed by students from the bachelor Computer Science
+at Utrecht University within the Software Project course.
+© Copyright Utrecht University 
+(Department of Information and Computing Sciences)
+"""
