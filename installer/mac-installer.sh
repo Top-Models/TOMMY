da162c56f58389bc84d312b80d65e12adca75dd5
Jasper Hofman <j.d.hofman@students.uu.nl>
2024-05-01 10:35:18 +0200

    Update script and add mac icon for tommy

--- installer/mac-installer.sh
+++ installer/mac-installer.sh
@@ -18,26 +18,33 @@ pyinstaller \
 --windowed \
 --onedir \
 --name "${APP_NAME}" \
---icon "assets/tommyBackground.icns" \
+--icon "assets/tommy.icns" \
 --add-data "${DATA_FOLDER_PATH}/csv_files/*.csv:./data" \
 --add-data "${DATA_FOLDER_PATH}/stopwords.txt:./preprocessing_data" \
 --add-data "${DATA_FOLDER_PATH}/pipeline_download:./preprocessing_data/pipeline_download" \
 tommy/main.py
 
+# PyInstaller automatically does ad-hoc signing? (as need to --force)
+codesign --force --verbose --verify --sign - "${SOURCE_FOLDER_PATH}"
 echo "> Creating a DMG file for Tommy"
-
 create-dmg \
 --volname "${VOLUME_NAME}" \
---volicon "assets/tommyBackground.icns" \
---background "assets/img.png" \
+--volicon "assets/tommy.icns" \
+--background "assets/installerBackground.png" \
 --window-pos 200 120 \
---window-size 800 600 \
+--window-size 835 680 \
 --icon-size 128 \
---icon "${APP_NAME}.app" 230 250 \
+--icon "${APP_NAME}.app" 220 285 \
 --hide-extension "${APP_NAME}.app" \
---app-drop-link 593 250 \
+--app-drop-link 603 285 \
 --no-internet-enable \
 "${DMG_FOLDER_PATH}" \
 "${SOURCE_FOLDER_PATH}"
 
-echo "> You can find the DMG file in the dist folder"
+# Should be able to only sign the disk image?
+echo "> Signing the DMG file"
+
+#codesign --force --verify --verbose --sign "Developer ID Application: Tommy" "${DMG_FOLDER_PATH}"
+#codesign --verify --verbose=4 --deep --strict "${DMG_FOLDER_PATH}"
+
+echo "> You can find the signed DMG file in the dist folder"
