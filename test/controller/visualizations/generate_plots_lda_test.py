6811269511f1c8a7d75817d8ed4df93b0f27eaab
Thochril <tcloef@gmail.com>
2024-05-02 16:56:53 +0200

    Add correlation matrix test

--- test/controller/visualizations/generate_plots_lda_test.py
+++ test/controller/visualizations/generate_plots_lda_test.py
@@ -1,8 +1,12 @@
 import pytest
 from gensim import models
+
+from tommy.controller.topic_modelling_runners.abstract_topic_runner import (
+    TopicRunner)
 from tommy.controller.topic_modelling_runners.lda_runner import LdaRunner
+from tommy.controller.visualizations.correlation_matrix_creator import (
+    CorrelationMatrixCreator)
 from tommy.model.topic_model import TopicModel
-from tommy.controller.visualizations import correlation_matrix_creator
 
 
 @pytest.fixture
@@ -10,5 +14,17 @@ def gensim_lda_model():
     model = models.LdaModel.load('../../test_data/test_lda_model/lda_model')
     return model
 
-def test_generate_plots(gensim_lda_model):
-    assert gensim_lda_model.print_topics()
+@pytest.fixture
+def lda_runner(gensim_lda_model, mocker):
+    topic_model = TopicModel()
+    mocker.patch.object(LdaRunner, 'train_model')
+    lda_runner = LdaRunner(topic_model, [], 0, 0)
+    lda_runner._model = gensim_lda_model
+    return lda_runner
+
+
+def test_generate_correlation_matrix(gensim_lda_model, lda_runner):
+    correlation_matrix = CorrelationMatrixCreator()
+    figure = correlation_matrix.get_figure(lda_runner)
+    #figure.show()
+    assert figure
