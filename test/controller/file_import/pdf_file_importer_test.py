4903377089473ed34ceb319a0d3c086e4c0b11df
Belle-and-the-Wolf <91776815+Belle-and-the-Wolf@users.noreply.github.com>
2024-05-02 12:24:26 +0200

    Added test data and started writing tests for pdf_file_importer.py

---
+++ test/controller/file_import/pdf_file_importer_test.py
@@ -0,0 +1,20 @@
+import pytest
+from tommy.controller.file_import.pdf_file_importer import PdfFileImporter
+
+class TestPdfFileImporter:
+    @pytest.fixture(autouse=True)
+    def setup(self):
+        self.pdf_importer = PdfFileImporter()
+
+    def test_compatible_file(self):
+        assert self.pdf_importer.compatible_file('test_pdf/correct_files/kattenverhaaltje 1.pdf')
+        assert not self.pdf_importer.compatible_file('test_pdf/incorrect_files/kattenverhaaltje 1.docx')
+
+    def test_load_file(self):
+        for testfile in self.pdf_importer.load_file('test_pdf/correct_files'):
+            file_generator = self.pdf_importer.load_file(testfile)
+            file = next(file_generator)
+            assert file is not None
+
+    def test_generate_file(self):
+        #TODO: Implement test
