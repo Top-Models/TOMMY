eac8647c06cda6ac9a0732831de25f8c2361a362
Belle-and-the-Wolf <91776815+Belle-and-the-Wolf@users.noreply.github.com>
2024-05-02 12:51:01 +0200

    Refactored the test to follow the csv_file_importer better

--- test/controller/file_import/pdf_file_importer_test.py
+++ test/controller/file_import/pdf_file_importer_test.py
@@ -1,30 +1,56 @@
+import os
 import pytest
 from tommy.controller.file_import.pdf_file_importer import PdfFileImporter
 
-class TestPdfFileImporter:
-    @pytest.fixture(autouse=True)
-    def setup(self):
-        self.pdf_importer = PdfFileImporter()
 
-    def test_compatible_file(self):
-        assert self.pdf_importer.compatible_file('test_pdf/'
-                                                 'correct_files/'
-                                                 'kattenverhaaltje 1.pdf')
-        assert not self.pdf_importer.compatible_file('test_pdf/'
-                                                     'incorrect_files/'
-                                                     'kattenverhaaltje 1.docx')
+TEST_DATA_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                             '..',
+                                             '..',
+                                             '..',
+                                             'test',
+                                             'test_data',
+                                             'test_pdf_files'))
 
-    def test_load_file(self):
-        for testfile in self.pdf_importer.load_file('test_pdf/correct_files'):
-            file_generator = self.pdf_importer.load_file(testfile)
-            file = next(file_generator)
-            assert file is not None
 
-    def test_generate_file(self):
-        file = self.pdf_importer.generate_file('test',
-                                               'test_pdf/correct_files/kattenverhaaltje 1.pdf',
-                                               'metadata')
+@pytest.fixture
+def pdf_file_importer():
+    return PdfFileImporter()
+
+
+def test_compatible_file(pdf_file_importer):
+    compatible_path = os.path.join(TEST_DATA_DIR,
+                                   'correct_files',
+                                   'kattenverhaaltje 1.pdf')
+    assert pdf_file_importer.compatible_file(compatible_path)
+
+    incompatible_path = os.path.join(TEST_DATA_DIR,
+                                     'incorrect_files',
+                                     'kattenverhaaltje 1.docx')
+    assert not pdf_file_importer.compatible_file(incompatible_path)
+
+
+def test_load_file(pdf_file_importer):
+    files_path = os.path.join(TEST_DATA_DIR, 'correct_files')
+    for testfile in pdf_file_importer.load_file(files_path):
+        file_generator = pdf_file_importer.load_file(testfile)
+        file = next(file_generator)
         assert file is not None
-        assert file.body == 'test'
-        assert file.path == 'test_pdf/correct_files/kattenverhaaltje 1.pdf'
-        assert file.metadata == 'metadata'
+
+
+def test_generate_file(pdf_file_importer):
+    testfile_metadata = {
+             '/Author': 'Test Author',
+             '/Title': 'Test Title',
+             '/ModDate': 'D:20220101000000+00\'00\''}
+
+    file = pdf_file_importer.generate_file('test',
+                                               'test_pdf/'
+                                               'correct_files/'
+                                               'kattenverhaaltje 1.pdf',
+                                                metadata=testfile_metadata)
+    assert file is not None
+    assert file.body == 'test'
+    assert file.path == 'test_pdf/correct_files/kattenverhaaltje 1.pdf'
+    assert file.metadata == 'metadata'
+
+
