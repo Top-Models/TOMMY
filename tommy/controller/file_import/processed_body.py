13120a410c342faaa09ae65394b75b49e8510069
FyorDev <uuf@fyor.nl>
2024-05-07 19:23:49 +0200

    Add missing copyright licenses

--- tommy/controller/file_import/processed_body.py
+++ tommy/controller/file_import/processed_body.py
@@ -9,3 +9,11 @@ class ProcessedBody:
     document after pre-processing.
     """
     body: Any
+
+
+"""
+This program has been developed by students from the bachelor Computer Science
+at Utrecht University within the Software Project course.
+© Copyright Utrecht University
+(Department of Information and Computing Sciences)
+"""
