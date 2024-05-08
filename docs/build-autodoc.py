ebbb9b529d8e121dca3055a03a278583be595855
FyorDev <uuf@fyor.nl>
2024-05-08 10:19:20 +0200

    Sphinx if name == main

--- docs/build-autodoc.py
+++ docs/build-autodoc.py
@@ -3,24 +3,25 @@ import webbrowser
 import os
 import shutil
 
-# Define paths
-autosummary_path = os.path.join('docs', 'source', '_autosummary')
-build_path = os.path.join('docs', 'build')
+
+if __name__ == "__main__":
+    # Define paths
+    autosummary_path = os.path.join('docs', 'source', '_autosummary')
+    build_path = os.path.join('docs', 'build')
+
+    # Delete previous build
+    def delete_directory(path):
+        if os.path.exists(path):
+            print(f"Deleting {path}")
+            shutil.rmtree(path)
 
 
-# Delete previous build
-def delete_directory(path):
-    if os.path.exists(path):
-        print(f"Deleting {path}")
-        shutil.rmtree(path)
+    delete_directory(autosummary_path)
+    delete_directory(build_path)
 
-
-delete_directory(autosummary_path)
-delete_directory(build_path)
-
-subprocess.run(["sphinx-build", "-M", "html", "docs/source/",
-                "docs/build/"])
-webbrowser.open('file://' + os.path.abspath('./docs/build/html/index.html'))
+    subprocess.run(["sphinx-build", "-M", "html", "docs/source/",
+                    "docs/build/"])
+    webbrowser.open('file://' + os.path.abspath('./docs/build/html/index.html'))
 
 """
 This program has been developed by students from the bachelor Computer Science
