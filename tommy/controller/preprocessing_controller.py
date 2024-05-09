3a2f957357b772df53495bab2c100adbfd88fcb1
Jasper Hofman <j.d.hofman@students.uu.nl>
2024-05-09 12:50:46 +0200

    Fix line length

--- tommy/controller/preprocessing_controller.py
+++ tommy/controller/preprocessing_controller.py
@@ -15,14 +15,15 @@ class PreprocessingController:
 
     def __init__(self) -> None:
         pipeline_path = os.path.join(
-            application_settings.preprocessing_data_folder,
-            "pipeline_download",
-            "nl_core_news_sm-3.7.0"
-        )
-        nlp = spacy.load(pipeline_path, exclude=["tagger", "attribute_ruler", "parser", "senter"])
+            application_settings.preprocessing_data_folder
+            , "pipeline_download", "nl_core_news_sm-3.7.0")
+        nlp = spacy.load(pipeline_path,
+                         exclude=["tagger", "attribute_ruler", "parser",
+                                  "senter"])
         self._nlp = nlp
 
-        self._entity_categories = {"PERSON", "FAC", "LAW", "TIME", "PERCENT", "MONEY", "QUANTITY", "ORDINAL", "CARDINAL"}
+        self._entity_categories = {"PERSON", "FAC", "LAW", "TIME", "PERCENT",
+                                   "MONEY", "QUANTITY", "ORDINAL", "CARDINAL"}
         # TODO: refine the entity set (i.e. "proper-noun filtering")
         self._pos_categories = {"NOUN", "PROPN", "ADJ", "ADV", "VERB"}
 
@@ -44,22 +45,32 @@ class PreprocessingController:
         :return list[str]: The processed tokens
         """
         # 2, 3, 4 - all steps that require token-level information
-        lemmas = [token.lemma_ for token in doc if token.ent_type_ not in self._entity_categories and token.pos_ in self._pos_categories]
-        # TODO: look at pos tags (i.e. fine-grained pos i/p coarse-grained pos)
+        lemmas = [token.lemma_ for token in doc if
+                  token.ent_type_ not in self._entity_categories and
+                  token.pos_ in self._pos_categories]
+        # TODO: look at pos tags (i.e. fine-grained pos i/o coarse-grained pos)
 
         # 5 - transforming the tokens (lemmas) themselves
         lemmas = [lemma.lower() for lemma in lemmas if len(lemma) > 3]
-        # TODO: fine-grain abbreviation filtering (i.e. don't exclude every token under 4 characters)
+        # TODO: fine-grain abbreviation filtering (i.e. don't exclude every
+        #  token under 4 characters)
 
-        # TODO: fix "-"words and remove diacritical marks (i.e. character 'normalization')
+        # TODO: fix "-"words and remove diacritical marks
+        #  (i.e. character 'normalization')
 
-        # TODO: 6,7
-
-        # 8 - stopword removal and synonym replacement
+        lemmas = self.apply_synonyms(lemmas)
         lemmas = self.filter_stopwords(lemmas)
-        #lemmas = self.replace_synonyms(lemmas)
 
         return lemmas
+
+    def apply_synonyms(self, tokens: list[str]) -> list[str]:
+        """
+        Applies synonyms to the given list of tokens.
+
+        :param tokens: The list of tokens
+        :return: The list of tokens where tokens are mapped to their synonyms
+        """
+        return [token for token in tokens]
 
     def filter_stopwords(self, tokens: list[str]) -> list[str]:
         """
@@ -69,9 +80,6 @@ class PreprocessingController:
         :return: The list of tokens without stopwords
         """
         return [token for token in tokens if token not in self._stopwords_model]
-
-    #def replace_synonyms(self, tokens: list[str]) -> list[str]:
-        # todo
 
 
 """
