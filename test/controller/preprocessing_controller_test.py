cc908b0509acdd39c6762eacc059ac0660d30a40
Jasper Hofman <j.d.hofman@students.uu.nl>
2024-05-20 11:53:17 +0200

    Refactor preprocessing tests

--- test/controller/preprocessing_controller_test.py
+++ test/controller/preprocessing_controller_test.py
@@ -13,16 +13,18 @@ def preprocessing_controller():
 def stopwords_model():
     return StopwordsModel()
 
+
 @pytest.fixture
 def synonyms_model():
     return SynonymsModel()
 
 
-def test_process_text(preprocessing_controller, stopwords_model,synonyms_model):
+def test_process_text(preprocessing_controller, stopwords_model,
+                      synonyms_model):
     """
     Test the process_text method of PreprocessingController.
     """
-    preprocessing_controller.set_model_refs(stopwords_model,synonyms_model)
+    preprocessing_controller.set_model_refs(stopwords_model, synonyms_model)
     text = "Dit is een test zin token2."
     tokens = preprocessing_controller.process_text(text)
     assert isinstance(tokens, list)
@@ -39,11 +41,12 @@ def test_process_text(preprocessing_controller, stopwords_model,synonyms_model):
     assert "token2" in tokens
 
 
-def test_process_tokens(preprocessing_controller, stopwords_model,synonyms_model):
+def test_process_tokens(preprocessing_controller, stopwords_model,
+                        synonyms_model):
     """
     Test the process_tokens method of PreprocessingController.
     """
-    preprocessing_controller.set_model_refs(stopwords_model,synonyms_model)
+    preprocessing_controller.set_model_refs(stopwords_model, synonyms_model)
     text = "Dit is een test zin token2."
     doc = preprocessing_controller._nlp(text)
     tokens = preprocessing_controller.process_tokens(doc)
@@ -61,11 +64,12 @@ def test_process_tokens(preprocessing_controller, stopwords_model,synonyms_model
     assert "test" in tokens
 
 
-def test_filter_stopwords(preprocessing_controller, stopwords_model,synonyms_model):
+def test_filter_stopwords(preprocessing_controller, stopwords_model,
+                          synonyms_model):
     """
     Test the filter_stopwords method of PreprocessingController.
     """
-    preprocessing_controller.set_model_refs(stopwords_model,synonyms_model)
+    preprocessing_controller.set_model_refs(stopwords_model, synonyms_model)
     tokens = ["dit", "is", "een", "test", "zin"]
     filtered_tokens = preprocessing_controller.filter_stopwords(tokens)
     assert isinstance(filtered_tokens, list)
--- test/controller/stopwords_controller_test.py
+++ test/controller/stopwords_controller_test.py
@@ -3,8 +3,9 @@ from PySide6.QtCore import Qt
 from PySide6.QtWidgets import QApplication
 from PySide6.QtTest import QTest
 from tommy.controller.stopwords_controller import StopwordsController
+from tommy.controller.synonyms_controller import SynonymsController
 from tommy.model.stopwords_model import StopwordsModel
-from tommy.view.preprocessing_view import StopwordsView
+from tommy.view.preprocessing_view import PreprocessingView
 
 
 @pytest.fixture
@@ -22,30 +23,35 @@ def stopwords_controller():
 
 
 @pytest.fixture
+def synonyms_controller():
+    """Fixture for creating a SynonymsController."""
+    return SynonymsController()
+
+
+@pytest.fixture
 def stopwords_model():
     """Fixture for creating a StopwordsModel."""
     return StopwordsModel()
 
 
 @pytest.fixture
-def stopwords_view(stopwords_controller):
+def preprocessing_view(stopwords_controller, synonyms_controller):
     """Fixture for creating a StopwordsView."""
-    view = StopwordsView(stopwords_controller)
+    view = PreprocessingView(stopwords_controller, synonyms_controller)
     return view
 
 
-def test_stopwords_view_model_linkage(stopwords_controller,stopwords_view, stopwords_model, qtbot):
+def test_stopwords_view_model_linkage(stopwords_controller, preprocessing_view,
+                                      stopwords_model, qtbot):
     """Test whether adding stopwords in the view updates the model."""
-    # Set up the view and model
-    stopwords_view.show()
     stopwords_controller.set_model_refs(stopwords_model)
 
     # Add a stopword in the view
     test_stopword = "test_stopword"
-    qtbot.keyClicks(stopwords_view.blacklist_tab, test_stopword)
+    qtbot.keyClicks(preprocessing_view.blacklist_tab, test_stopword)
 
     # Simulate enter key press to trigger the update
-    QTest.keyPress(stopwords_view.blacklist_tab, Qt.Key_Return)
+    QTest.keyPress(preprocessing_view.blacklist_tab, Qt.Key_Return)
 
     # Check if the stopword is added to the model
     assert test_stopword in stopwords_model.extra_words
