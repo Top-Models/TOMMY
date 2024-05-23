3de9f225be99e3e19656eb694b6991fc69622e10
Mees <meesoke@hotmail.com>
2024-05-23 14:59:03 +0200

    Fix bug where after selecting the combobox, it still allows you to scroll
    even better combobox

--- tommy/view/settings_view/abstract_settings/better_combo_box.py
+++ tommy/view/settings_view/abstract_settings/better_combo_box.py
@@ -6,10 +6,6 @@ from PySide6.QtWidgets import QComboBox
 class BetterComboBox(QComboBox):
     def __init__(self):
         super().__init__()
-        self.setFocusPolicy(Qt.StrongFocus)
 
     def wheelEvent(self, event: QWheelEvent):
-        if not self.hasFocus():
-            event.ignore()
-            return
-        super().wheelEvent(event)
+        event.ignore()
