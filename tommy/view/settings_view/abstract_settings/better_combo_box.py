4b81cbe15cabed120230bcd1960ba31a53acf8af
SirPatrin <49597347+SirPatrin@users.noreply.github.com>
2024-05-23 12:57:08 +0200

    Disabled scrolling on dropdown menus

---
+++ tommy/view/settings_view/abstract_settings/better_combo_box.py
@@ -0,0 +1,15 @@
+from PySide6.QtCore import Qt
+from PySide6.QtGui import QWheelEvent
+from PySide6.QtWidgets import QWidget, QComboBox
+
+
+class BetterComboBox(QComboBox):
+    def __init__(self):
+        super().__init__()
+        self.setFocusPolicy(Qt.StrongFocus)
+
+    def wheelEvent(self, event: QWheelEvent):
+        if not self.hasFocus():
+            event.ignore()
+            return
+        super().wheelEvent(event)
