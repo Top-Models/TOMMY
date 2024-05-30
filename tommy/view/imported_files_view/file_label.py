fa8b75d459cb3e793cdd70da18a8379f6a17b28b
FyorDev <uuf@fyor.nl>
2024-05-30 17:23:24 +0200

    Add bar to show document topic correspondence

--- tommy/view/imported_files_view/file_label.py
+++ tommy/view/imported_files_view/file_label.py
@@ -1,12 +1,12 @@
 from PySide6.QtCore import Qt, Signal
-from PySide6.QtGui import QMouseEvent
+from PySide6.QtGui import QMouseEvent, QPainter, QColor
 from PySide6.QtWidgets import QLabel, QSizePolicy
 
 from tommy.controller.file_import.metadata import Metadata
 from tommy.support.constant_variables import (
     heading_font, label_height,
     text_font, medium_light_gray, hover_medium_light_gray,
-    pressed_medium_light_gray)
+    pressed_medium_light_gray, seco_col_blue)
 
 
 class FileLabel(QLabel):
@@ -25,6 +25,7 @@ class FileLabel(QLabel):
 
         super().__init__(file_metadata.name, parent)
         self.file = file_metadata
+        self.topic_correspondence = topic_correspondence
 
         if file_metadata.title is not None:
             self.setText(file_metadata.title)
@@ -46,7 +47,7 @@ class FileLabel(QLabel):
 
         # If provided, set the correspondence with the current selected topic
         if topic_correspondence is not None:
-            self.setText(f"{str(100*topic_correspondence)[:4]}% - "
+            self.setText(f"{str(100 * topic_correspondence)[:4]}% - "
                          f"{self.file.name}")
 
     def enterEvent(self, event):
@@ -145,6 +146,21 @@ class FileLabel(QLabel):
                                f"padding: 3px;")
         self.clicked.emit(self)
         super().mouseReleaseEvent(event)
+
+    def paintEvent(self, event) -> None:
+        """
+        Add a green bar to the label, sized to the document topic correspondence
+        :param event: The paint event
+        :return: None
+        """
+        super().paintEvent(event)
+        if self.topic_correspondence is not None:
+            painter = QPainter(self)
+            bar_width = int(self.width() * self.topic_correspondence)
+            bar_height = 7
+            painter.fillRect(0, self.height() - bar_height, bar_width,
+                             bar_height, QColor(seco_col_blue))
+            painter.end()
 
 
 """
