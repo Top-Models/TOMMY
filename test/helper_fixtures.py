2ed810aa394199635be8a106977ba7a78cd888a0
Mees <meesoke@hotmail.com>
2024-06-01 18:13:41 +0200

    Add docstring to controller_no_pipeline fixture

--- test/helper_fixtures.py
+++ test/helper_fixtures.py
@@ -5,7 +5,13 @@ from tommy.controller.controller import Controller
 
 
 @pytest.fixture
-def controller_no_pipeline(mocker: MockerFixture):
+def controller_no_pipeline(mocker: MockerFixture) -> Controller:
+    """
+    Fixture for a Controller that doesn't load the preprocessing pipeline.
+    Loading the pipeline takes a long time and is not necessary for most
+    tests, therefore most tests that require an instance of the Controller
+    can use this one instead.
+    """
     with mocker.patch('tommy.controller.preprocessing_controller'
                       '.PreprocessingController.load_pipeline'):
         return Controller()
