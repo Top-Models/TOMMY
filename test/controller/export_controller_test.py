367756ed1d6243f3833d01e1e6b96dc97d861b52
ErbenKlaver <erbenjerbenjk@gmail.com>
2024-06-02 16:23:30 +0200

    added export doc topics csv

--- test/controller/export_controller_test.py
+++ test/controller/export_controller_test.py
@@ -5,6 +5,15 @@ import os
 
 from tommy.controller.export_controller import ExportController
 from tommy.controller.graph_controller import GraphController
+from tommy.controller.topic_modelling_controller import \
+    TopicModellingController
+
+# Mock data
+DOCUMENT_TOPICS = [
+    ('doc1', [0.1, 0.9]),
+    ('doc2', [0.3, 0.7]),
+    ('doc3', [0.4, 0.6]),
+]
 
 
 @pytest.fixture
@@ -19,9 +28,17 @@ def mock_graph_controller():
 
 
 @pytest.fixture
-def export_controller(mock_graph_controller):
+def mock_topic_modelling_controller():
+    mock_controller = MagicMock(spec=TopicModellingController)
+    return mock_controller
+
+
+@pytest.fixture
+def export_controller(mock_graph_controller, mock_topic_modelling_controller):
     controller = ExportController()
-    controller.set_controller_refs(mock_graph_controller)
+    controller.set_controller_refs(mock_graph_controller,
+                                   mock_topic_modelling_controller)
+    controller.document_topics = DOCUMENT_TOPICS
     return controller
 
 
@@ -40,3 +57,31 @@ def test_export_topic_words_csv(export_controller, tmp_path):
     assert rows[4] == ['Topic 2', 'word4', '0.6']
 
     os.remove(temp_csv_path)
+
+
+def test_export_document_topics_csv(export_controller, tmp_path):
+    temp_csv_path = tmp_path / "document_topics.csv"
+    export_controller.export_document_topics_csv(temp_csv_path)
+
+    with open(temp_csv_path, mode='r') as file:
+        csv_reader = csv.reader(file)
+        rows = list(csv_reader)
+
+    # Verify header row
+    assert rows[0] == ['Document ID', 'Topic 1 Probability',
+                       'Topic 2 Probability']
+
+    # Verify document topic rows
+    assert rows[1] == ['doc1', '0.1', '0.9']
+    assert rows[2] == ['doc2', '0.3', '0.7']
+    assert rows[3] == ['doc3', '0.4', '0.6']
+
+    os.remove(temp_csv_path)
+
+
+"""
+This program has been developed by students from the bachelor Computer Science
+at Utrecht University within the Software Project course.
+© Copyright Utrecht University
+(Department of Information and Computing Sciences)
+"""
