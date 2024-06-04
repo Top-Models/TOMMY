e0b6e80058b9cda8184f418744912afe855f6df2
Belle-and-the-Wolf <91776815+Belle-and-the-Wolf@users.noreply.github.com>
2024-06-04 11:31:54 +0200

    Wrote tests for the custom_name model

---
+++ test/custom_name_model_tests.py
@@ -0,0 +1,59 @@
+import pytest
+from tommy.model.custom_name_model import TopicNameModel
+
+
+def test_initialization_with_config_name():
+    model = TopicNameModel(config_name="new config")
+    assert model.topic_names == {"new config": {}}
+
+
+def test_initialization_without_config_name():
+    model = TopicNameModel()
+    assert model.topic_names == {}
+
+
+def test_get_topic_name_with_existing_config_and_index():
+    model = TopicNameModel()
+    model.set_topic_name("test config",
+                         0,
+                         "test topic")
+    assert (model.get_topic_name("test config", 0) ==
+            "test topic")
+
+
+# Returns default topic
+def test_get_topic_name_with_non_existing_config():
+    model = TopicNameModel()
+    assert (model.get_topic_name("new test config", 0) ==
+            "Topic 1")
+
+
+def test_get_topic_name_with_non_existing_index():
+    model = TopicNameModel()
+    model.set_topic_name("config", 0, "custom topic")
+    assert model.get_topic_name("config", 1) == "Topic 2"
+
+
+def test_set_topic_name():
+    model = TopicNameModel()
+    model.set_topic_name("config", 0, "custom topic")
+    assert model.topic_names["config"][0] == "custom topic"
+
+
+def test_remove_config():
+    model = TopicNameModel()
+    model.set_topic_name("config", 0, "topic")
+    model.remove_config("config")
+    assert "config" not in model.topic_names
+
+
+def test_remove_non_existing_config():
+    model = TopicNameModel()
+    model.set_topic_name("config1", 0, "topic")
+    model.remove_config("config2")
+    assert "config1" in model.topic_names
+
+
+# Running the tests
+if __name__ == "__main__":
+    pytest.main()
