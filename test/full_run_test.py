396fd8b059cb55bddd779b88928a7fb141338095
FyorDev <uuf@fyor.nl>
2024-06-04 15:41:45 +0200

    Mock async_worker for test

--- test/full_run_test.py
+++ test/full_run_test.py
@@ -1,4 +1,8 @@
 import pytest
+from unittest.mock import patch
+
+from PySide6 import QtCore
+from PySide6.QtCore import Signal
 
 from tommy.controller.controller import Controller
 
@@ -8,9 +12,26 @@ def controller() -> Controller:
     return Controller()
 
 
+class BlockingWorker(QtCore.QObject):
+    """
+    Mocked async worker, so the tests don't run async.
+    """
+    finished = Signal()
+
+    def __init__(self, func):
+        super().__init__()
+        self.func = func
+
+    def start(self):
+        self.func()
+        self.finished.emit()
+
+
 def test_run_without_error(controller: Controller) -> None:
-    # test if topic modelling can run without error
-    controller.project_settings_controller.set_input_folder_path(
-        "../test/test_data/test_pdf_files/correct_files")
-    controller.on_run_topic_modelling()
-    assert controller.graph_controller.has_topic_runner is True
+    with patch('tommy.controller.topic_modelling_controller.Worker',
+               new=BlockingWorker):
+        # test if topic modelling can run without error
+        controller.project_settings_controller.set_input_folder_path(
+            "../test/test_data/test_pdf_files/correct_files")
+        controller.on_run_topic_modelling()
+        assert controller.graph_controller.has_topic_runner is True
