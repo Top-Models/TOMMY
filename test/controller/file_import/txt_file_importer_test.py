ccfea442e40bde2d2dbeb223828f378e27e2b3ba
SirPatrin <wessel.van.der.ven@gmail.com>
2024-06-18 21:10:52 +0200

    Added txt file support

---
+++ test/controller/file_import/txt_file_importer_test.py
@@ -0,0 +1,92 @@
+import os
+import zipfile
+
+import pytest
+from datetime import datetime, timedelta
+
+from tommy.controller.file_import.txt_file_importer import TxtFileImporter
+
+
+TEST_DATA_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                             '..',
+                                             '..',
+                                             '..',
+                                             'test',
+                                             'test_data',
+                                             'test_txt_files'))
+
+
+@pytest.fixture
+def txt_file_importer():
+    return TxtFileImporter()
+
+
+def test_compatible_file(txt_file_importer):
+    compatible_path = os.path.join(TEST_DATA_DIR,
+                                   'correct_files',
+                                   'kattenverhaaltje 1.txt')
+    assert txt_file_importer.compatible_file(compatible_path)
+
+    incompatible_path = os.path.join(TEST_DATA_DIR,
+                                     'incorrect_files',
+                                     'kattenverhaaltje 1.pdf')
+    assert not txt_file_importer.compatible_file(incompatible_path)
+
+
+# you can still open a txt file when it is corrupted, the only tell is the
+# text becoming gibberish. We will leave detection of faulty txt files to
+# the user.
+
+# def test_corrupted_file(txt_file_importer):
+#     corrupted_path = os.path.join(TEST_DATA_DIR,
+#                                   'corrupt_files',
+#                                   'hondenverhaaltje 1.txt')
+#     with pytest.raises(zipfile.BadZipFile) as exception_info:
+#         txt_file_importer.compatible_file(corrupted_path)
+
+
+def test_load_file(txt_file_importer):
+    testfile = os.path.join(TEST_DATA_DIR,
+                            'correct_files',
+                            'kattenverhaaltje 2.txt')
+
+    for file_generator in txt_file_importer.load_file(testfile):
+        file_text = file_generator.body.body.strip()
+
+        # Assert
+        assert file_generator is not None
+        assert file_text == "Verhaaltje over een kat"
+
+
+def test_generate_file(txt_file_importer):
+    filepath = os.path.join(TEST_DATA_DIR,
+                            'correct_files',
+                            'kattenverhaaltje 1.txt')
+
+    file_date = datetime.now()
+
+    file = txt_file_importer.generate_file("Verhaaltje over een kat",
+                                            path=filepath)
+
+    # Set date as current date for testing purposes
+    file.metadata.date = file_date
+
+    assert file is not None
+    assert file.metadata is not None
+    assert file.body.body == "Verhaaltje over een kat"
+    assert file.metadata.title == 'kattenverhaaltje 1'
+
+    # Get the current date
+    current_date = datetime.now()
+
+    # Check if the date in the metadata is validly set (within 1 day)
+    # We are interested in days only, so we usually ignore time
+    assert abs(file.metadata.date - current_date) < timedelta(minutes=10)
+
+
+"""
+This program has been developed by students from the bachelor Computer Science
+at Utrecht University within the Software Project course.
+© Copyright Utrecht University
+(Department of Information and Computing Sciences)
+"""
---
+++ tommy/controller/file_import/txt_file_importer.py
@@ -0,0 +1,91 @@
+import os
+from typing import Generator
+from datetime import datetime
+
+from tommy.controller.file_import import file_importer_base
+from tommy.controller.file_import.metadata import Metadata
+from tommy.controller.file_import.raw_body import RawBody
+from tommy.controller.file_import.raw_file import RawFile
+
+
+class TxtFileImporter(file_importer_base.FileImporterBase):
+    """
+    Handles importing of txt files
+    """
+
+    def __init__(self) -> None:
+        """
+        Initializes a new instance of the class.
+        """
+        pass
+
+    def compatible_file(self, path: str) -> bool:
+        """
+        A txt file is compatible with this parser if and only if
+        it can be opened and read without errors.
+
+        :param path: The string path to the txt file to be checked for
+                     compatibility.
+
+        :return: bool: True if the file is compatible, False otherwise.
+        """
+
+        if not path.endswith('.txt'):
+            return False
+
+        # Since txt files are perfectly happy being read even when their
+        # text isn't interpretable, needlessly opening can be avoided
+        # with open(path, "rb") as txtFile:
+        #     txtFile.read()
+        return True
+
+    def load_file(self, path: str) -> Generator[RawFile, None, None]:
+        """
+        Loads a txt file and yields a File object.
+
+        :param path: The string path to the txt file.
+        :return: Generator[RawFile, None, None]: A generator yielding File objects.
+        """
+        with open(path, "r", encoding='utf-8-sig') as txtFile:
+            text = txtFile.read()
+            yield self.generate_file(text, path)
+
+    @staticmethod
+    def generate_file(text: str, path) -> RawFile:
+        """
+        Generates a File object from a txt file.
+
+        :param text: A string representing the text of the txt file.
+        :param path: The string path to the txt file.
+        :return: A RawFile object generated from the txt file
+        containing metadata and the raw text of the file.
+        """
+
+        alt_title = os.path.basename(path).replace('.txt', '')
+
+        try:
+            mod_time = os.path.getmtime(path)
+            file_date = datetime.fromtimestamp(mod_time)
+        except Exception:
+            # If unable to get the modification time, don't set a date
+            file_date = None
+
+        return RawFile(
+            metadata=Metadata(author=None,
+                              title=alt_title,
+                              date=file_date,
+                              url=None,
+                              path=path,
+                              format="txt",
+                              length=len(text.split(" ")),
+                              name=alt_title,
+                              size=os.stat(path).st_size),
+            body=RawBody(body=text))
+
+
+"""
+This program has been developed by students from the bachelor Computer Science
+at Utrecht University within the Software Project course.
+© Copyright Utrecht University
+(Department of Information and Computing Sciences)
+"""
