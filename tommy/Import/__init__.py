3d94bfcecdb6c5a56660511295680910887456b1
TemporarilyTired <66777276+TemporarilyTired@users.noreply.github.com>
2024-06-19 16:53:20 +0200

    Removed View

--- tommy/Import/__init__.py
+++ tommy/Import/__init__.py
@@ -1,6 +1,4 @@
 """
-View
-
 This program has been developed by students from the bachelor Computer Science
 at Utrecht University within the Software Project course.
 © Copyright Utrecht University
